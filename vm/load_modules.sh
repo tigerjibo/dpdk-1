b31134569b63c7d74c65d5950b8cdc81acdd39d7
slank-vaio-arch <slank.dev@gmail.com>
2016-05-10 23:40:48 +0900

    [INIT] use ubuntu vagrant-box

--- vm/load_modules.sh
+++ vm/load_modules.sh
@@ -2,7 +2,7 @@
 
 modprobe uio_pci_generic
 # modprobe uio
-# insmod /home/vagrant/Desktop/dpdk/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
-# insmod /home/vagrant/Desktop/dpdk/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
+# insmod /home/vagrant/dpdk-16.04/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+# insmod /home/vagrant/dpdk-16.04/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
 
 modprobe vfio-pci
--- vm/mount_hugetlbfs.sh
+++
@@ -1,4 +0,0 @@
-
-
-mkdir -p /mnt/huge
-echo "nodev /mnt/huge hugetlbfs defaults 0 0" >> /etc/fstab
