2ddba42ce6d829c0943c735bcb5a60174b57be7e
slank-osx <slank.dev@gmail.com>
2016-05-15 01:57:39 +0900

    [CLEAN] some codes edited at 2016.05.15-01:57

--- myapp/02_packet_capture/main.c
+++ myapp/02_packet_capture/main.c
@@ -2,54 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <inttypes.h>
+
 #include <rte_version.h>
+#include <rte_eal.h>
 #include <rte_ethdev.h>
 #include <rte_ether.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+#include <rte_mbuf.h>
+
 
 #define DEBUG(s) fprintf(stderr, "[%s:%d] %s\n", __func__, __LINE__, s)
 
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 32
 
-static void print_info_port(int port_id)
+
+
+static int port_init(uint8_t port, struct rte_mempool* mbuf_pool)
 {
-
-	if (!rte_eth_dev_is_valid_port(port_id))
-		rte_exit(EXIT_FAILURE, "rte_eth_dev_is_valid_port() failed\n");
-
-    printf("-PortID:%d--------------------\n", port_id);
-
-	struct rte_eth_dev_info dev_info;
-	memset(&dev_info, 0, sizeof(dev_info));
-	rte_eth_dev_info_get(port_id, &dev_info);
-	printf("PCI Info: %04x:%02x:%02x.%x \n",
-		dev_info.pci_dev->addr.domain, dev_info.pci_dev->addr.bus,
-		dev_info.pci_dev->addr.devid, dev_info.pci_dev->addr.function);
-
-
-    struct ether_addr addr;
-	rte_eth_macaddr_get(port_id, &addr);
-
-    printf("HW Addr : ");
-    int i;
-    for (i=0; i<6; i++) {
-        printf("%02x", addr.addr_bytes[i]);
-        if (i<5) printf(":");
-    }
-    printf("\n");
-    printf("-----------------------------\n");
-    
+    printf("port number: %d \n", port);
+    if (mbuf_pool == NULL) 
+        exit(-1);
+    return 0;
 }
 
-
-static void print_info_all(void)
-{
-    uint32_t num_ports = rte_eth_dev_count();
-    printf("%u NICs found. \n", num_ports);
-
-    uint32_t i;
-    for (i=0; i<num_ports; i++) {
-        print_info_port(i);
-    }
-}
 
 
 int main(int argc, char** argv)
@@ -61,9 +40,31 @@ int main(int argc, char** argv)
 
     printf("\n\n\n");
     DEBUG("start");
-    print_info_all();
+
+    uint32_t num_ports = rte_eth_dev_count();
+    printf("%d ports found  \n", num_ports);
+    if (num_ports < 1)
+        rte_exit(EXIT_FAILURE, "rte_eth_dev_count()");
+    
+    struct rte_mempool* mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * num_ports, 
+            MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());  
+    if (mbuf_pool == NULL)
+        rte_exit(EXIT_FAILURE, "rtembuf_pool_create()");
+
+    uint8_t portid;
+    for (portid=0; portid < num_ports; portid++) {
+        if (port_init(portid, mbuf_pool) != 0)
+            rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu8 "\n", portid);
+    }
+
+    if (rte_lcore_count() > 1) {
+        printf("WARNING: Too many lcores enabled. Only 1 used. \n");
+    }
+
     DEBUG("finish");
-
-
     return 0;
 }
+
+
+
+
